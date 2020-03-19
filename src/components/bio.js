94cd18c64071f67da9e8a6cd3cfdf9110715aa02
Tim Simon <timothee@hackages.io>
2020-03-19 20:02:48 +0100

    fixed twitter

--- src/components/bio.js
+++ src/components/bio.js
@@ -59,8 +59,8 @@ const Bio = () => {
       <p>
         Written by <strong>{author.name}</strong> {author.summary}
         {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+        <a href={`https://twitter.com/${social.mastodon}`}>
+          You should follow him on Mastodon
         </a>
       </p>
     </div>
