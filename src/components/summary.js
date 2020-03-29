a990cbdb024e95a7fe060262c090d5026390a871
Tim Simon <timothee@hackages.io>
2020-03-29 23:05:41 +0200

    better summary

--- src/components/summary.js
+++ src/components/summary.js
@@ -1,11 +1,13 @@
 import styled from "styled-components"
 
 const Summary = styled.h4`
-  margin: 50px 0px;
+  margin: 50px 50px;
   font-size: 34px;
   font-weight: normal;
   color: #2e3440;
   font-family: "Lustria";
+  max-width: 1000px;
+  text-align: center;
 `
 
 export default Summary
