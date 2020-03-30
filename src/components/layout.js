1f4dc9a2c98120a584de921bf02010f8faad3340
Tim Simon <timothee@hackages.io>
2020-03-30 13:13:02 +0200

    fixing horizontal overlay

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,32 @@
 import React from "react"
 import Header from "./header"
+import styled from "styled-components"
+
+const Footer = styled.footer`
+  width: 100vw;
+  text-align: right;
+  padding-right: 20px;
+  box-sizing: border-box;
+`
+
+const Main = styled.main`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 50px;
+`
+
+const Wrapper = styled.div`
+  font-family: Lora;
+`
 
 const Layout = ({ title, children }) => (
   <>
     <Header>{title}</Header>
-    <div style={{ fontFamily: "Lora" }}>
-      <main
-        style={{
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "column",
-          padding: "50px",
-        }}
-      >
-        {children}
-      </main>
-      <footer style={{ margin: 20 }}>
-        © {new Date().getFullYear()}, by Tim Simon
-      </footer>
-    </div>
+    <Wrapper>
+      <Main style={{}}>{children}</Main>
+      <Footer>© {new Date().getFullYear()}, by Tim Simon</Footer>
+    </Wrapper>
   </>
 )
 
